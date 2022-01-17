7cd270a203207c001874bd8298254a363a6f98ec
Xavier333322211 <76964252+Xavier333322211@users.noreply.github.com>
2022-01-16 19:02:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var food = 0;
-function Click(number){document.getElementById("cookies").innerHTML = cookies;
-    food = food + number;
+function Click(number){document.getElementById("cookies").innerHTML = food;
+    food = food + 1;
 };
+var wood = 0;
+function Click(number){document.getElementById("cookies").innerHTML = wood;
+    wood = wood + 1;
